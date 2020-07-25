437b2dee7c7928110202a56e85dbfce972deaa8e
athanasios <ath.kouk.oid@gmail.com>
2020-07-25 13:13:42 +0200

    bug fix

---
+++ src/de/hft/stuttgart/citygmlinjector/contract/IActionHandle.java
@@ -0,0 +1,9 @@
+package de.hft.stuttgart.citygmlinjector.contract;
+
+import de.hft.stuttgart.citygmlinjector.state.Context;
+
+public interface IActionHandle {
+	
+	public void handle(Context context);
+
+}
